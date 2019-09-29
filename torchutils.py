ce98cae1a8a16c9fa4d1ea40238a352ddd52875d
sraashis <sraashis@gmail.com>
2019-09-29 16:29:03 -0400

    Weight sampler added.

--- torchutils.py
+++ torchutils.py
@@ -14,6 +14,20 @@ def safe_collate(batch):
     return default_collate([b for b in batch if b])
 
 
+def make_weights_for_balanced_classes(images, nclasses):
+    count = [0] * nclasses
+    for item in images:
+        count[item[1]] += 1
+    weight_per_class = [0.] * nclasses
+    N = float(sum(count))
+    for i in range(nclasses):
+        weight_per_class[i] = N / float(count[i])
+    weight = [0] * len(images)
+    for idx, val in enumerate(images):
+        weight[idx] = weight_per_class[val[1]]
+    return weight
+
+
 class NNDataLoader(DataLoader):
 
     def __init__(self, **kw):
