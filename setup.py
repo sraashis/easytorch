423199df1f4deae6d8d4874db2e89459aa4f0ad6
Aashis Khanal <sraashis@gmail.com>
2020-07-28 01:58:47 -0400

    V 1.0 quenn.

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ README = (HERE / "README.md").read_text()
 
 # This call to setup() does all the work
 setup(
-    name="quenn-sraashis",
-    version="1.12",
+    name="quenn",
+    version="1.0",
     description="Quick Neural Network Experimentation",
     long_description=README,
     long_description_content_type="text/markdown",
