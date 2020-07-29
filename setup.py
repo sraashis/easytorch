f2834ff1ccefaba7ecc3cefb1bf5bd56fcaef6bd
Aashis Khanal <sraashis@gmail.com>
2020-07-28 22:31:55 -0400

    Mixed precision added.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="quenn",
-    version="1.133",
+    version="1.3",
     description="Quick Neural Network Experimentation",
     long_description=README,
     long_description_content_type="text/markdown",
