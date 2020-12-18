3839e915e5c7505d31b8937c51bf6136c7aab13b
Aashis Khanal <sraashis@gmail.com>
2020-12-18 18:19:43 -0500

    Version bump

---
+++ easytorch/data/data.py
@@ -0,0 +1,116 @@
+import json as _json
+import os as _os
+
+from torch.utils.data import DataLoader as _DataLoader, Dataset as _Dataset
+from torch.utils.data._utils.collate import default_collate as _default_collate
+
+
+def safe_collate(batch):
+    r"""
+    Savely select batches/skip errors in file loading.
+    """
+    return _default_collate([b for b in batch if b])
+
+
+class ETDataLoader(_DataLoader):
+
+    def __init__(self, **kw):
+        super(ETDataLoader, self).__init__(**kw)
+
+    @classmethod
+    def new(cls, **kw):
+        _kw = {
+            'dataset': None,
+            'batch_size': 1,
+            'sampler': None,
+            'shuffle': False,
+            'batch_sampler': None,
+            'num_workers': 0,
+            'pin_memory': False,
+            'drop_last': False,
+            'timeout': 0,
+            'worker_init_fn': None
+        }
+        for k in _kw.keys():
+            _kw[k] = kw.get(k, _kw.get(k))
+        return cls(collate_fn=safe_collate, **_kw)
+
+
+class ETDataset(_Dataset):
+    def __init__(self, mode='init', limit=float('inf')):
+        self.mode = mode
+        self.limit = limit
+        self.dataspecs = {}
+        self.indices = []
+
+    def load_index(self, dataset_name, file):
+        r"""
+        Logic to load indices of a single file.
+        -Sometimes one image can have multiple indices like U-net where we have to get multiple patches of images.
+        """
+        self.indices.append([dataset_name, file])
+
+    def _load_indices(self, dataset_name, files, **kw):
+        r"""
+        We load the proper indices/names(whatever is called) of the files in order to prepare minibatches.
+        Only load lim numbr of files so that it is easer to debug(Default is infinite, -lim/--load-lim argument).
+        """
+        for file in files:
+            if len(self) >= self.limit:
+                break
+            self.load_index(dataset_name, file)
+
+        if kw.get('verbose', True):
+            print(f'{dataset_name}, {self.mode}, {len(self)} Indices Loaded')
+
+    def __getitem__(self, index):
+        r"""
+        Logic to load one file and send to model. The mini-batch generation will be handled by Dataloader.
+        Here we just need to write logic to deal with single file.
+        """
+        raise NotImplementedError('Must be implemented by child class.')
+
+    def __len__(self):
+        return len(self.indices)
+
+    @property
+    def transforms(self):
+        return None
+
+    def add(self, files, debug=True, **kw):
+        r"""
+        An extra layer for added flexibility.
+        """
+        self.dataspecs[kw['name']] = kw
+        self._load_indices(dataset_name=kw['name'], files=files, debug=debug)
+
+    @classmethod
+    def pool(cls, args, dataspecs, split_key=None, load_sparse=False):
+        r"""
+        This method takes multiple dataspecs and pools the first splits of all the datasets.
+        So that we can train one single model on all the datasets. It will automatically refer correct data files,
+            no need to move files in single folder.
+        """
+        all_d = []
+        for dspec in dataspecs:
+            for split in _os.listdir(dspec['split_dir']):
+                split = _json.loads(open(dspec['split_dir'] + _os.sep + split).read())
+                if load_sparse:
+                    for file in split[split_key]:
+                        if len(all_d) >= args['load_limit']:
+                            break
+                        d = cls(mode=split_key)
+                        d.add(files=[file], debug=False, **dspec)
+                        all_d.append(d)
+                    if args['verbose']:
+                        print(f'{len(all_d)} sparse dataset loaded.')
+                else:
+                    if len(all_d) <= 0:
+                        all_d.append(cls(mode=split_key, limit=args['load_limit']))
+                    all_d[0].add(files=split[split_key], debug=args['verbose'], **dspec)
+                """
+                Pooling only works with 1 split at the moment.
+                """
+                break
+
+        return all_d
---
+++ easytorch/data/datautils.py
@@ -0,0 +1,113 @@
+import json as _json
+import os as _os
+import random as _rd
+
+import numpy as _np
+
+_sep = _os.sep
+
+
+def create_ratio_split(images, save_to_dir=None, ratio: dict = (0.6, 0.2, 0.2), first_key='train', name='SPLIT'):
+    keys = [first_key]
+    if len(ratio) == 2:
+        keys.append('test')
+    elif len(ratio) == 3:
+        keys.append('validation')
+        keys.append('test')
+
+    _ratio = ratio[::-1]
+    locs = _np.array([sum(_ratio[0:i + 1]) for i in range(len(ratio) - 1)])
+    locs = (locs * len(images)).astype(int)
+    splits = _np.split(images[::-1], locs)[::-1]
+    splits = dict([(k, sp.tolist()[::-1]) for k, sp in zip(keys, splits)])
+    if save_to_dir:
+        f = open(save_to_dir + _sep + f'{name}.json', "w")
+        f.write(_json.dumps(splits))
+        f.close()
+    else:
+        return splits
+
+
+def create_k_fold_splits(files, k=0, save_to_dir=None, shuffle_files=True, name='SPLIT'):
+    if shuffle_files:
+        _rd.shuffle(files)
+
+    ix_splits = _np.array_split(_np.arange(len(files)), k)
+    for i in range(len(ix_splits)):
+        test_ix = ix_splits[i].tolist()
+        val_ix = ix_splits[(i + 1) % len(ix_splits)].tolist()
+        train_ix = [ix for ix in _np.arange(len(files)) if ix not in test_ix + val_ix]
+
+        splits = {'train': [files[ix] for ix in train_ix],
+                  'validation': [files[ix] for ix in val_ix],
+                  'test': [files[ix] for ix in test_ix]}
+
+        if save_to_dir:
+            f = open(save_to_dir + _sep + name + str(i) + '.json', "w")
+            f.write(_json.dumps(splits))
+            f.close()
+        else:
+            return splits
+
+
+def uniform_mix_two_lists(smaller, larger, shuffle=True):
+    if shuffle:
+        _rd.shuffle(smaller)
+        _rd.shuffle(larger)
+
+    len_smaller, len_larger = len(smaller), len(larger)
+
+    accumulator = []
+    while len(accumulator) < len_smaller + len_larger:
+        try:
+            for i in range(int(len_larger / len_smaller)):
+                accumulator.append(larger.pop())
+        except Exception:
+            pass
+        try:
+            accumulator.append(smaller.pop())
+        except Exception:
+            pass
+
+    return accumulator
+
+
+def make_weights_for_balanced_classes(images, nclasses):
+    count = [0] * nclasses
+    for item in images:
+        count[item[1]] += 1
+    weight_per_class = [0.] * nclasses
+    N = float(sum(count))
+    for i in range(nclasses):
+        weight_per_class[i] = N / float(count[i])
+    weight = [0] * len(images)
+    for idx, val in enumerate(images):
+        weight[idx] = weight_per_class[val[1]]
+    return weight
+
+
+def create_splits_(log_dir, dspec):
+    if dspec.get('split_dir') and _os.path.exists(dspec.get('split_dir')) and len(list(
+            _os.listdir(dspec.get('split_dir')))) > 0:
+        return False
+
+    dspec['split_dir'] = log_dir + _sep + 'splits'
+    if _os.path.exists(dspec['split_dir']) and len(list(_os.listdir(dspec['split_dir']))) > 0:
+        return False
+
+    _os.makedirs(dspec['split_dir'], exist_ok=True)
+    return True
+
+
+def _init_kfolds(dspec, args):
+    r"""
+    Initialize k-folds for given dataspec.
+        If: custom splits path is given it will use the splits from there
+        else: will create new k-splits and run k-fold cross validation.
+    """
+    if args.get('num_fold'):
+        create_k_fold_splits(_os.listdir(dspec['data_dir']), k=args['num_folds'],
+                             save_to_dir=dspec['split_dir'], shuffle_files=True, name=dspec['name'])
+    else:
+        create_ratio_split(_os.listdir(dspec['data_dir']),
+                           save_to_dir=dspec['split_dir'], ratio=args['split_ratio'], name=dspec['name'])
