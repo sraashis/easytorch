69316758c5f756fcaf317ced555c0f676c58577f
Aashis Khanal <sraashis@gmail.com>
2021-01-07 01:35:40 -0500

    Shortened and version bumped.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="easytorch",
-    version="1.7.0",
+    version="1.7.1",
     description="Easy Neural Network Experiments with pytorch",
     long_description=README,
     long_description_content_type="text/markdown",
