543798ea9316f1fa2ea7677645b98a964f3e187b
Aashis Khanal <sraashis@gmail.com>
2021-05-08 09:21:22 +0545

    Typing extension issue fixed.

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
     ],
     packages=['easytorch', 'easytorch.config', 'easytorch.data', 'easytorch.metrics', 'easytorch.utils', 'easytorch.vision'],
