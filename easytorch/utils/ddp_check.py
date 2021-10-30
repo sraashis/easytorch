975311f3964656a44dd2557c0bc3655d56b4c4f0
Aashis Khanal <sraashis@gmail.com>
2021-10-30 16:54:20 -0400

    DDP check.

--- easytorch/utils/ddp_check.py
+++ easytorch/utils/ddp_check.py
@@ -1,6 +1,6 @@
 import os as _os
 
-import torch
+import torch as _torch
 import torch.distributed as _dist
 import torch.multiprocessing as _mp
 import torch.nn as _nn
@@ -8,9 +8,7 @@ import torch.optim as _optim
 from easytorch import default_args as _args
 
 
-def _gpu_process(rank, args, train_loader, model):
-    print(f'Rank:{rank} spawned...')
-    gpu = args['gpus'][rank]
+def _setup(rank, args):
     if not args.get('world_size'):
         args['world_size'] = args['num_gpus'] * args['num_nodes']
 
@@ -18,16 +16,23 @@ def _gpu_process(rank, args, train_loader, model):
     _dist.init_process_group(backend=args['dist_backend'],
                              init_method=args['init_method'],
                              rank=world_rank, world_size=args['world_size'])
-    """ *** Distributed stuff from here *** """
 
-    t = torch.Tensor([1]).to(gpu)
-    count = _dist.all_reduce(t, _dist.ReduceOp.SUM)
-    print(f'*** Total participation: {count} ***')
+
+def _ddp_train(rank, args, data_loaders, model):
+    gpu = args['gpus'][rank]
+    _setup(rank, args)
+    print(f'Rank {rank} initialized...')
+
+    t = _torch.Tensor([1]).to(gpu)
+    _dist.all_reduce(t, _dist.ReduceOp.SUM)
+    if rank == 0:
+        print(f"\n*** Total nodes: {args['num_nodes']} ***")
+        print(f'*** Total processes participation: {int(t.item())} ***')
 
     loss_fn = _nn.MSELoss()
     model = model.to(gpu)
     optimizer = _optim.SGD(model.parameters(), lr=0.001)
-    for batch in train_loader:
+    for batch in data_loaders['train']:
         data = batch[0].to(gpu)
         labels = batch[1].to(gpu)
         outputs = model(data)
@@ -55,15 +60,17 @@ def _run_distributed(args, model, data_loaders: dict):
 
     _os.environ['MASTER_ADDR'] = args['master_addr']
     _os.environ['MASTER_PORT'] = args['master_port']
-    _mp.spawn(_gpu_process, nprocs=len(args['gpus']), args=(args, data_loaders, model))
+    _mp.spawn(_ddp_train, nprocs=len(args['gpus']), args=(args, data_loaders, model))
 
 
-"""Run in cpu
+"""
+Run in cpu
 init_method = 'gloo'
 world_size = <desired number of processes>
 """
 
-"""Run in GPUs
+"""
+Run in GPUs
 init_method = 'nccl'
 world_size = None, since automatically determined by number of GPUs.
 """
