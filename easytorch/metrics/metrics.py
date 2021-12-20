0b23121910f0506a5b23fd00beee726943387c7b
Aashis Khanal <sraashis@gmail.com>
2021-12-20 13:58:10 -0500

    Metrics v2 + Readme++

--- easytorch/metrics/metrics.py
+++ easytorch/metrics/metrics.py
@@ -293,6 +293,7 @@ class AUCROCMetrics(ETMetrics):
         self.fpr = None
         self.tpr = None
         self.thresholds = None
+        self._auc = 0
 
     def accumulate(self, other):
         self.probabilities += other.probabilities
@@ -304,14 +305,15 @@ class AUCROCMetrics(ETMetrics):
 
     def auc(self):
         self.fpr, self.tpr, self.thresholds = _metrics.roc_curve(self.labels, self.probabilities, pos_label=1)
-        return _metrics.auc(self.fpr, self.tpr)
+        return max(_metrics.auc(self.fpr, self.tpr), self._auc)
 
     def get(self, *args, **kw) -> List[float]:
         return [round(self.auc(), self.num_precision)]
 
     def dist_gather(self, device='cpu'):
-        # Todo
-        pass
+        auc = _torch.from_numpy(_np.array([self.auc()])).to(device)
+        _dist.all_reduce(auc, op=_dist.ReduceOp.SUM)
+        self._auc = float(auc.item() / _dist.get_world_size())
 
     def add(self, pred: _torch.Tensor, true: _torch.Tensor):
         self.probabilities += pred.flatten().clone().detach().cpu().tolist()
